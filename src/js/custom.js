f4d03e4946409d1f87e477edf2909ecf2b4b8bea
TheFallenCol <germansilva1944@hotmail.com>
2020-02-16 13:06:24 -0500

    Componentes de menu y menu de dos niveles

--- src/js/custom.js
+++ src/js/custom.js
@@ -10,4 +10,23 @@ jQuery( document ).ready(function($) {
     expandIcon: '',
     collapseIcon: ''
   });
+  
+  $("#btn1").click(function() {
+	  alert("Prueba");
+     fontSizeChange('display-date', 3);
+	});
 });
+
+function fontSizeChange(elem, factor) {
+    if (!elem) {
+        return false;
+    }
+    else {
+        factor = factor || 2; // 2 is the default
+        elem = elem.nodeType && elem.nodeType === 1 ? elem : document.getElementById(elem);
+        var cur = window.getComputedStyle(elem, null).fontSize,
+            size = parseInt(cur.replace(/\D+/g,''),10),
+            unit = cur.replace(/\d+/g,'');
+        elem.style.fontSize = (size * factor) + unit;
+    }    
+}
